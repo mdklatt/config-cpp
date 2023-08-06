a4604b0158521877bf637d23b1c12ba066832e22
Michael Klatt <mdklatt@alumni.ou.edu>
2023-08-06 10:27:52 -0500

    Added Config base class

--- include/config/configure.hpp
+++ include/config/configure.hpp
@@ -17,133 +17,185 @@
 
 
 namespace configure {
+
+/**
+ * Abstract base class for configuration classes
+ */
+class Config {
+public:
     /**
-     * Store TOML config data.
+     * Load config data from an input stream.
      *
-     * Keys are hierarchical and specify a complete path to their target
-     * value using dotted components, *e.g.* "table.nested.value".
+     * @param stream input stream
+     * @param root place data at this root
      */
-    class TomlConfig {
-    public:
-        /**
-         * Default constructor.
-         */
-        TomlConfig() = default;
+    void load(std::istream& stream, const std::string& root="");
 
-        /**
-         * Construct a Config object from an input stream.
-         *
-         * @param stream TOML data stream
-         */
-        explicit TomlConfig(std::istream& stream);
+    /**
+     * Load config data from a file path.
+     *
+     * @param path input file path
+     * @param root place data at this root
+     */
+    void load(const std::filesystem::path& path, const std::string& root="");
 
-        /**
-         * Construct a Config object from a file.
-         *
-         * @param path TOML file path
-         */
-        explicit TomlConfig(const std::filesystem::path& path);
+    /**
+     * Writeable access to a value.
+     *
+     * A new value node will be created if it does not exist, including all
+     * parent nodes as necessary. An existing value must already have the
+     * correct type or a `std::invalid_argument` exception will be thrown.
+     *
+     * @tparam T value type (long long, double, bool, or std::string)
+     * @param key hierarchical key
+     * @return value reference
+     */
+    template<typename T>
+    T& at(const std::string& key);
 
-        /**
-         * Load config data from an input stream.
-         *
-         * @param stream TOML data stream
-         * @param root place data at this root
-         */
-        void load(std::istream& stream, const std::string& root="");
+    /**
+     * Read-only access to a value.
+     *
+     * A `std::invalid_argument` exception will be thrown if the target is
+     * not an existing value of the correct type.
+     *
+     * @tparam T value type (long long, double, bool, or std::string)
+     * @param key hierarchical key
+     */
+    template<typename T>
+    const T& at(const std::string& key) const;
 
-        /**
-         * Load config data from a file path.
-         *
-         * @param path TOML file path
-         * @param root place data at this root
-         */
-        void load(const std::filesystem::path& path, const std::string& root="");
+    /**
+     * Test if key exists.
+     *
+     * @param key hierarchical key
+     * @return true if key exists
+     */
+    bool has_key(const std::string& key) const;
 
-        /**
-         * Writeable access to a value.
-         *
-         * A new value node will be created if it does not exist, including all
-         * parent nodes as necessary. An existing value must already have the
-         * correct type or a `std::invalid_argument` exception will be thrown.
-         *
-         * @tparam T value type (long long, double, bool, or std::string)
-         * @param key hierarchical key
-         * @return value reference
-         */
-        template<typename T>
-        T& at(const std::string& key);
+protected:
+    Config() = default;
 
-        /**
-         * Read-only access to a value.
-         *
-         * A `std::invalid_argument` exception will be thrown if the target is
-         * not an existing value of the correct type.
-         *
-         * @tparam T value type (long long, double, bool, or std::string)
-         * @param key hierarchical key
-         */
-        template<typename T>
-        const T& at(const std::string& key) const;
+    /**
+     * Parse input from a stream.
+     *
+     * @param stream input stream
+     * @return TOML data structure
+     */
+    virtual toml::table parse(std::istream& stream) = 0;
 
-        /**
-         * Test if key exists.
-         *
-         * @param key hierarchical key
-         * @return true if key exists
-         */
-        bool has_key(const std::string& key) const;
-        
-    private:
-        static constexpr char keydel{'.'};
-        toml::table tree;
+    /**
+     * Parse input from a file.
+     *
+     * @param path input file path
+     * @return TOML data structure
+     */
+    virtual toml::table parse(const std::filesystem::path& path) = 0;
 
-        /**
-         * Private implementation of at().
-         *
-         * @tparam T value type
-         * @param key hierarchical key
-         * @param type node type
-         * @return value reference
-         */
-        template <typename T>
-        T& at(const std::string& key, const toml::node_type& type);
+private:
+    static constexpr char keydel{'.'};
+    toml::table tree;
 
-        /**
-         * Private implementation of at().
-         *
-         * @tparam T native C++ value type
-         * @param key hierarchical key
-         * @param type node type
-         * @return value reference
-         */
-        template <typename T>
-        const T& at(const std::string& key, const toml::node_type& type) const;
+    /**
+     * Load data from a data structure.
+     *
+     * @param table TOML data structure
+     * @param root place data at this root
+     */
+    void load(const toml::table&& table, const std::string& root);
 
-        /**
-         * Insert a new value node at the given location.
-         *
-         * Parent nodes are created as necessary. A `std::invalid_argument`
-         * exception is thrown if a parent node exists and is not a table or if
-         * the target node exists and has the correct type.
-         *
-         * @param T native C++ value type
-         * @param key hierarchical key
-         */
-        template <typename T>
-        void insert(const std::string& key);
+    /**
+     * Insert a new value node at the given location.
+     *
+     * Parent nodes are created as necessary. A `std::invalid_argument`
+     * exception is thrown if a parent node exists and is not a table or if
+     * the target node exists and has the correct type.
+     *
+     * @param T value type
+     * @param key hierarchical key
+     */
+    template <typename T>
+    void insert(const std::string& key);
 
-        /**
-         * Insert a new table node at the given location.
-         *
-         * Parent nodes are created as necessary. A `std::invalid_argument`
-         * exception is thrown if a parent node exists and is not a table or if
-         * the target node exists and is not a table.
-         *
-         * @param key hierarchical key
-         */
-        toml::table& insert_table(const std::string& key);
-    };
+    /**
+     * Insert a new table node at the given location.
+     *
+     * Parent nodes are created as necessary. A `std::invalid_argument`
+     * exception is thrown if a parent node exists and is not a table or if
+     * the target node exists and is not a table.
+     *
+     * @param key hierarchical key
+     */
+    toml::table& insert_table(const std::string& key);
+
+    /**
+     * Private implementation of at().
+     *
+     * @tparam T value type
+     * @param key hierarchical key
+     * @param type node type
+     * @return value reference
+     */
+    template <typename T>
+    T& at(const std::string& key, const toml::node_type& type);
+
+    /**
+     * Private implementation of at().
+     *
+     * @tparam T native C++ value type
+     * @param key hierarchical key
+     * @param type node type
+     * @return value reference
+     */
+    template <typename T>
+    const T& at(const std::string& key, const toml::node_type& type) const;
+};
+
+
+/**
+ * Store TOML config data.
+ *
+ * Keys are hierarchical and specify a complete path to their target
+ * value using dotted components, *e.g.* "table.nested.value".
+ */
+class TomlConfig: public Config {
+public:
+    /**
+     * Default constructor.
+     */
+    TomlConfig() = default;
+
+    /**
+     * Construct a Config object from an input stream.
+     *
+     * @param stream TOML data stream
+     */
+    explicit TomlConfig(std::istream& stream);
+
+    /**
+     * Construct a Config object from a file.
+     *
+     * @param path TOML file path
+     */
+    explicit TomlConfig(const std::filesystem::path& path);
+
+protected:
+    /**
+     * Load config data from an input stream.
+     *
+     * @param stream TOML data stream
+     * @param root place data at this root
+     */
+    toml::table parse(std::istream& stream) override;
+
+    /**
+     * Load config data from a file path.
+     *
+     * @param path TOML file path
+     * @param root place data at this root
+     */
+    toml::table parse(const std::filesystem::path& path) override;
+};
 
 }  // namespace
 
--- src/configure.cpp
+++ src/configure.cpp
@@ -2,7 +2,6 @@
  * Implementation of the configure module.
  */
 #include "config/configure.hpp"
-#include <yaml-cpp/yaml.h>
 #include <istream>
 #include <stdexcept>
 
@@ -14,75 +13,63 @@ using std::string;
 using namespace configure;
 
 
-TomlConfig::TomlConfig(istream& stream) {
-    load(stream);
+void Config::load(istream& stream, const string& root) {
+    load(parse(stream), root);
 }
 
 
-TomlConfig::TomlConfig(const std::filesystem::path& path) {
-    load(path);
-}
-
-
-void TomlConfig::load(istream& stream, const string& root) {
-    const auto table{toml::parse(stream)};
-    if (root.empty()) {
-        tree = table;
-    }
-    else {
-        insert_table(root) = table;
-    }
-}
-
-
-void TomlConfig::load(const std::filesystem::path& path, const string& root) {
-    const auto table{toml::parse_file(path.string())};
-    if (root.empty()) {
-        tree = table;
-    }
-    else {
-        insert_table(root) = table;
-    }
+void Config::load(const std::filesystem::path& path, const string& root) {
+    load(parse(path.string()), root);
 }
 
 
 template <>
-bool& TomlConfig::at<>(const string& key) {
+bool& Config::at<>(const string& key) {
     return at<bool>(key, toml::node_type::boolean);
 }
 
 
 template <>
-double& TomlConfig::at<>(const string& key) {
+double& Config::at<>(const string& key) {
     return at<double>(key, toml::node_type::floating_point);
 }
 
 
 template <>
-long long& TomlConfig::at<>(const string& key) {
+long long& Config::at<>(const string& key) {
     return at<long long>(key, toml::node_type::integer);
 }
 
 
 template <>
-string& TomlConfig::at<>(const string& key) {
+string& Config::at<>(const string& key) {
     return at<string>(key, toml::node_type::string);
 }
 
 
 template <>
-const string& TomlConfig::at<>(const string& key) const {
+const string& Config::at<>(const string& key) const {
     return at<string>(key, toml::node_type::string);
 }
 
 
-bool TomlConfig::has_key(const string& key) const {
+bool Config::has_key(const string& key) const {
     return tree.at_path(key).type() != toml::node_type::none;
 }
 
 
+void Config::load(const toml::table&& table, const string& root) {
+    if (root.empty()) {
+        tree = table;
+    }
+    else {
+        insert_table(root) = table;
+    }
+}
+
+
 template <typename T>
-T& TomlConfig::at(const string& key, const toml::node_type& type) {
+T& Config::at(const string& key, const toml::node_type& type) {
     auto node{tree.at_path(key)};
     if (node.type() != type) {
         insert<T>(key);
@@ -93,7 +80,7 @@ T& TomlConfig::at(const string& key, const toml::node_type& type) {
 
 
 template <typename T>
-const T& TomlConfig::at(const string& key, const toml::node_type& type) const {
+const T& Config::at(const string& key, const toml::node_type& type) const {
     const auto node{tree.at_path(key)};
     if (node.type() != type) {
         throw invalid_argument{"incorrect type for node '" + key + "'"};
@@ -103,7 +90,7 @@ const T& TomlConfig::at(const string& key, const toml::node_type& type) const {
 
 
 template <typename T>
-void TomlConfig::insert(const std::string& key) {
+void Config::insert(const std::string& key) {
     const auto pos{key.rfind(keydel)};
     const auto parent{pos == string::npos ? "" : key.substr(0, pos)};
     const auto leaf{pos == string::npos ? key : key.substr(pos + 1)};
@@ -115,7 +102,7 @@ void TomlConfig::insert(const std::string& key) {
 }
 
 
-toml::table& TomlConfig::insert_table(const string& key) {
+toml::table& Config::insert_table(const string& key) {
     string parent;
     for (auto it{key.begin()}; it != key.end(); ++it) {
         // Create parent nodes.
@@ -139,4 +126,24 @@ toml::table& TomlConfig::insert_table(const string& key) {
         tree.at_path(parent).ref<toml::table>().emplace(leaf, toml::table());
     }
     return tree.at_path(key).ref<toml::table>();
+}
+
+
+TomlConfig::TomlConfig(istream& stream) {
+    load(stream);
+}
+
+
+TomlConfig::TomlConfig(const std::filesystem::path& path) {
+    load(path);
+}
+
+
+toml::table TomlConfig::parse(istream& stream) {
+    return toml::parse(stream);
+}
+
+
+toml::table TomlConfig::parse(const std::filesystem::path& path) {
+    return toml::parse_file(path.string());
 }
